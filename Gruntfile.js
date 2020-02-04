36639f25b945be21744ad331da7f0b019c0a30ef
Nilambar Sharma <nilambar@outlook.com>
2020-02-04 16:55:24 +0545

    Clean up packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,27 +41,6 @@ module.exports = function( grunt ) {
 				expand: true,
 				dot: true
 			}
-		},
-
-		// Update text domain.
-		addtextdomain: {
-			options: {
-				textdomain: '<%= options.text_domain %>',
-				updateDomains: true
-			},
-			target: {
-				files: {
-					src: [
-					'*.php',
-					'**/*.php',
-					'!node_modules/**',
-					'!vendor/**',
-					'!deploy/**',
-					'!build/**',
-					'!tests/**'
-					]
-				}
-			}
 		}
 
 	});
@@ -69,18 +48,11 @@ module.exports = function( grunt ) {
 	// Load NPM tasks to be used here.
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
-	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 
 	// Register tasks.
 	grunt.registerTask( 'default', [] );
 
-	grunt.registerTask( 'build', [
-		'textdomain'
-	]);
-
-	grunt.registerTask( 'textdomain', [
-		'addtextdomain'
-	]);
+	grunt.registerTask( 'build', [] );
 
 	grunt.registerTask( 'deploy', [
 		'clean:deploy',
