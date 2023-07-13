7c2d01b65a5716fc6ca9c135e6174af43d6f2945
Nilambar Sharma <nilambar@outlook.com>
2023-07-13 13:21:13 +0545

    feat: rest api

--- src/PostsApp.js
+++ src/PostsApp.js
@@ -18,20 +18,15 @@ class PostsApp extends React.Component {
 	};
 
 	componentDidMount() {
-		const data = new FormData();
-
 		this.setState( { loading: true } );
 
-		data.append( 'action', DTN_POSTS.posts_action );
-
-		fetch( DTN_POSTS.ajax_url, {
-			method: 'POST',
+		fetch( DTN_POSTS.rest_url, {
+			method: 'GET',
 			credentials: 'same-origin',
-			body: data,
 		} )
 			.then( ( response ) => response.json() )
-			.then( ( resp ) => {
-				this.setState( { postLists: resp.data, loading: false } );
+			.then( ( postLists ) => {
+				this.setState( { postLists, loading: false } );
 			} );
 	}
 
